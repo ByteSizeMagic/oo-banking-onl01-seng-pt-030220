04eaceac48dfbff0cb494fca6c3972fee4d6ff6c
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-01 12:04:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,6 +1,6 @@
 class Transfer
 
-  attr_reader 
+  attr_reader
   attr attr_accessor :transfer_from, :transfer_to, :transfer_amount, :status
 
   def initialize(transfer_from, transfer_to, transfer_amount)
